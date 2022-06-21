bb7bda065cc52b31e2226c78a0eda4155308413e
NataliePankovaOd <64199993+NataliePankovaOd@users.noreply.github.com>
2022-06-21 22:20:27 +0200

    Update controls.jsx

--- controls/controls.jsx
+++ controls/controls.jsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import setTimeFormat from '../script.js';
 
+export const Controls = ({
+  time,
+  start,
+  stop,
+  reset,
+  wait,
+}) => (
+  <>
+    <div className="main">
+        <div className="container">
+            <button type="button" className="button" onClick={start}>Start</button>
+            <button type="button" className="button" onClick={stop}>Stop</button>
+            <button type="button" className="button" onClick={reset}>Reset</button>
+            <button type="button" className="button" onClick={wait}>Wait</button>
+        </div>
+    </div>
+  </>
+);
+
+Controls.propTypes = {
+    time: PropTypes.number.isRequired,
+    start: PropTypes.func.isRequired,
+    stop: PropTypes.func.isRequired,
+    reset: PropTypes.func.isRequired,
+    wait: PropTypes.func.isRequired,
+};
